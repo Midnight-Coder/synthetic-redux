d39a3103d40794388e253f98c7889a04c14bbf4e
Sagar Jhobalia <sagar.jhobalia@gmail.com>
2021-09-12 21:09:28 -0700

    Adds parcel bundler, jest and babel config.

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "rootDir": "./test",
+  "moduleFileExtensions": [
+    "js",
+    "jsx"
+  ],
+  "moduleDirectories": [
+    "node_modules",
+    "src"
+  ],
+  "verbose": true
+}

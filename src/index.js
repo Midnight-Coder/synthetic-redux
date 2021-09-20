ff4a98a494ae7916314f3c4be7a3c8bbb453e034
Sagar Jhobalia <sagar.jhobalia@gmail.com>
2021-09-20 02:20:14 -0700

    Tries out parcel

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import SyntheticActions from './SyntheticActions';
+import SyntheticReducer from './SyntheticReducer';
+import SyntheticRedux from './SyntheticRedux';
+import SyntheticSagaEffect from './SyntheticSagaEffect';
+
+
+export {
+  SyntheticActions,
+  SyntheticReducer,
+  SyntheticRedux,
+  SyntheticSagaEffect,
+};

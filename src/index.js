6263a2387c583a2fed934c3f7094aa157fb34d76
Saurabh Nemade <saurabhnemade@users.noreply.github.com>
2021-09-22 18:51:18 +0530

    microbundle added. export corrected

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import SyntheticActions from './SyntheticActions';
 import SyntheticReducer from './SyntheticReducer';
 import SyntheticRedux from './SyntheticRedux';
 import SyntheticSagaEffect from './SyntheticSagaEffect';
+import { CATEGORY, CATEGORY_NAMES, ERROR_SUFFIX, SUCCESS_SUFFIX} from "./constants";
 
 
 export {
+  CATEGORY,
+  CATEGORY_NAMES,
+  ERROR_SUFFIX,
+  SUCCESS_SUFFIX,
   SyntheticActions,
   SyntheticReducer,
   SyntheticRedux,
